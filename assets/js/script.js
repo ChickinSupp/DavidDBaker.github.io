f41f1dc71f87a319238adf3cca79d0e91e10c5c6
O Padeiro <daviiddamiianbaker@gmail.com>
2020-05-27 00:33:35 -0400

    adding way point

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
 
-    $('h1').click(function() {
-        $(this).css('background-color', '#ff0000');
-    });
+    $('.js--section-features').waypoints(function(direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky')
+        } else {
+            $('nav').removeClass('sticky')
+        }
+    })
 
-});
+
+
+
+})
