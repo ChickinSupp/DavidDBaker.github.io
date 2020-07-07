2bb0c5fb6074065718ac09ca968e0897a0ae196b
David <daviiddamiianbaker@gmail.com>
2020-07-06 23:32:44 -0400

    button fixed, nav scroll added

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,14 +18,27 @@ $(document).ready(() => {
         $('html, body').animate({scrollTop: $('.js--section-plan').offset().top}, 1000)
     });
 
-    //button not working
-    $('.js--scroll-to-start').click((e) => {
-        e.preventDefault()
-        $('html, body').animate({scrollTop: $('js--section-features').offset().top}, 1000)
-    });
-
-
-    $('.')
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+     });
+     
+     
+     /* Navigation scroll */
+     $(function() {
+       $('a[href*=#]:not([href=#])').click(function() {
+         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+           var target = $(this.hash);
+           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+               scrollTop: target.offset().top
+             }, 1000);
+             return false;
+           }
+         }
+       });
+     });
+ 
 
 
 
