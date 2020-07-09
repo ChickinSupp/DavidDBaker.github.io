19ea404216e4aa47ead8820444c28d74c12f3001
O Padeiro <daviiddamiianbaker@gmail.com>
2020-07-08 22:40:00 -0400

    not working at the moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,26 +24,23 @@ $(document).ready(() => {
      
      
      /* Navigation scroll */
-     $(function() {
-       $('a[href*=#]:not([href=#])').click(function() {
-         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-           let target = $(this.hash);
-           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-               scrollTop: target.offset().top
-             }, 1000);
-             return false;
-           }
-         }
-       });
-     });
+    //  $(function() {
+    //    $('a[href*=#]:not([href=#])').click(function() {
+    //      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    //        let target = $(this.hash);
+    //        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    //        if (target.length) {
+    //          $('html,body').animate({
+    //            scrollTop: target.offset().top
+    //          }, 1000);
+    //          return false;
+    //        }
+    //      }
+    //    });
+    //  });
  
 
 
-
-
-    
 
 });
 
